d38ab249a8d84acdce7a5cd7c8f60b7ad4359f1d
antipers <antipers@gmail.com>
2022-06-29 01:16:45 +0300

    v.2 with negative numbers

--- task8/Program.cs
+++ task8/Program.cs
@@ -2,8 +2,19 @@
 int num = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("___");
 int line = 0;
-while (line < num)
+if (num > 0)
 {
-    Console.WriteLine($"{line + 2}");
-    line = line + 2;
+    while (line < num)
+    {
+        Console.WriteLine($"{line + 2}");
+        line = line + 2;
+    }
+}
+else
+{
+    while (line > num)
+    {
+        Console.WriteLine($"{line - 2}");
+        line = line - 2;
+    }
 }
