96b668665160a457d5b3a1a75e92dd5ac2372244
antipers <antipers@gmail.com>
2022-06-29 00:31:07 +0300

    completed task4

---
+++ task4/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите три числа- ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+int Num2 = Convert.ToInt32(Console.ReadLine());
+int Num3 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+int max = Num1;
+if (Num2 > max) max = Num2;
+if (Num3 > max) max = Num3;
+System.Console.WriteLine($"{max} - максимальное число");
