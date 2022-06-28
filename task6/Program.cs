e2b5bf53c602fff6002d71b3aa032b92347a7b31
antipers <antipers@gmail.com>
2022-06-29 00:37:27 +0300

    rapidly completed task6

---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите число- ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine();
+
+if (Num%2 == 0) System.Console.WriteLine($"{Num} - четное число");
+else
+{
+    System.Console.WriteLine($"{Num} - нечетное число");
+}
